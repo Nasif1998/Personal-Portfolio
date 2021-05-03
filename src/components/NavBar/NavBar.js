8604f1f232cff82445e445af7da040b6b2187fc9
Nasif123 <nasif.reza@northsouth.edu>
2021-05-03 12:51:50 +0600

    navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -34,7 +34,7 @@ const NavBar = () => {
 
         <nav class="navbar navbar-expand-lg navbar-light navColor">
             <div class="container-fluid">
-                <a class="navbar-brand" href="/home">Navbar</a>
+                <a class="navbar-brand" href="https://www.linkedin.com/in/nasif-reza-146177211/">Nasif</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
